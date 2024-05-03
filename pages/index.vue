82f015e7c4a2dbf7e536e7d4aa4bfd4feae8e96b
eeee0717 <chentao020717Work@outlook.com>
2024-05-03 22:21:27 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useMouse } from '@vueuse/core';
+
+
+const {x, y} = useMouse()
+
+</script>
+
+<template>
+  <div>
+   hello world {{ x }},{{ y }}
+  </div>
+</template>
