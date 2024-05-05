72d27df9da361a7bc4cb8d0aa7fb2899d3ca603d
eeee0717 <chentao020717Work@outlook.com>
2024-05-05 20:07:16 +0800

    feat:获取完整的正则表达式

--- pages/regexp/index.vue
+++ pages/regexp/index.vue
@@ -2,15 +2,30 @@
 // 是否自动匹配
 const auto = ref(true)
 const regStr = ref('')
-const regFlag = ref([])
+const regFlag = ref<string[]>([])
+const regTempFlag = ref<RegFlag[]>([])
 const reg = ref<RegExp | null>(null)
 watchEffect(() => {
   try {
     reg.value = regStr.value ? new RegExp(regStr.value, regFlag.value.join('')) : null
+    console.log(reg.value)
   }
   catch (e) {
     reg.value = null
   }
+})
+interface RegFlag {
+  label: string
+  value: string
+}
+const regFlagList: RegFlag[] = [
+  { label: '-g 全局匹配', value: 'g' },
+  { label: '-i 忽略大小写', value: 'i' },
+  { label: '-m 多行匹配', value: 'm' },
+  { label: '-s 特殊字符', value: 's' },
+]
+watchEffect(() => {
+  regFlag.value = regTempFlag.value.map(item => item.value)
 })
 </script>
 
@@ -21,20 +36,27 @@ watchEffect(() => {
       <UTooltip :text="`点击${auto ? '关闭' : '开启'}预设正则自动匹配`">
         <UButton
           class="justify-center items-center"
-          color="primary" size="sm" variant="outline" :icon="`${auto ? 'i-carbon-flash' : 'i-carbon-flash-off'}`"
+          color="primary" variant="outline" :icon="`${auto ? 'i-carbon-flash' : 'i-carbon-flash-off'}`"
           @click="auto = !auto"
         />
       </UTooltip>
 
-      <UInput v-model="regStr" size="md" placeholder="请输入正则表达式">
+      <UInput v-model="regStr" placeholder="请输入正则表达式">
         <template #leading>
           <UIcon :class="`${!reg ? 'color-amber' : 'color-emerald'}`" :name="`${!reg ? 'i-carbon-warning-alt' : 'i-carbon-checkmark-outline'}`" dynamic />
           <span class="ml-5px">/</span>
         </template>
         <template #trailing>
-          <span class="ml-5px">/</span>
+          <span class="ml-5px">/{{ reg?.flags || regFlag?.join('') }}</span>
         </template>
       </UInput>
+
+      <USelectMenu v-model="regTempFlag" class="w-35" :options="regFlagList" multiple>
+        <template #label>
+          <UIcon name="i-carbon-filter" dynamic />
+          <span>筛选修饰符</span>
+        </template>
+      </USelectMenu>
     </div>
     <!-- 中 -->
     <div class="mt-20px bg-[var(--color-fill-2)]">
