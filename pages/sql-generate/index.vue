33afb3f5fa25c4f8b5d30c85b964753902f276e5
eeee0717 <chentao020717Work@outlook.com>
2024-05-12 10:13:34 +0800

    chore: add description

--- pages/sql-generate/index.vue
+++ pages/sql-generate/index.vue
@@ -5,6 +5,20 @@ const selectedModel = ref<string>('')
 const selectAIService = ref<AIService>({ name: '', apiUrl: '', apiKey: '', selectedModel: '', models: [], checked: false })
 const content = ref('')
 const response = ref('')
+const sqlCommand = ref(`
+  - **SELECT** - 从数据库中提取数据
+  - **UPDATE** - 更新数据库中的数据
+  - **DELETE** - 从数据库中删除数据
+  - **INSERT INTO** - 向数据库中插入新数据
+  - **CREATE DATABASE** - 创建新数据库
+  - **ALTER DATABASE** - 修改数据库
+  - **CREATE TABLE** - 创建新表
+  - **ALTER TABLE** - 变更（改变）数据库表
+  - **DROP TABLE** - 删除表
+  - **CREATE INDEX** - 创建索引（搜索键）
+  - **DROP INDEX** - 删除索引
+`)
+
 watchEffect(() => {
   selectAIService.value = aiServices.value.find((aiService: AIService) => aiService.models.includes(selectedModel.value)) || { name: '', apiUrl: '', apiKey: '', selectedModel: '', models: [], checked: false }
 })
@@ -44,8 +58,8 @@ async function generateSQL() {
         </MDC>
       </div>
     </div>
-    <div class="b b-1">
-      hello
+    <div class="b-l border-base">
+      <MDC class="m-4 flex items-center justify-center" :value="sqlCommand" tag="article" />
     </div>
   </div>
 </template>
