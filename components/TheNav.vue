714559d348673de8ff86a0a1878a6e6703c6ec2e
eeee0717 <chentao020717Work@outlook.com>
2024-05-15 15:00:34 +0800

    chore: avatar base

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+const isAuth = ref(false)
+const user = useSupabaseUser()
+const avatarUrl = ref('')
+watchEffect(() => {
+  const userMeta = JSON.parse(JSON.stringify(user.value?.user_metadata))
+  if (user.value) {
+    avatarUrl.value = userMeta.avatar_url
+    isAuth.value = true
+  }
+})
+</script>
+
 <template>
   <nav px6 py4 text-lg border="b base" flex="~ gap-2 items-center">
     <NuxtLink
@@ -20,5 +33,13 @@
     >
       <div i-carbon-logo-github text-2xl />
     </NuxtLink>
+    <div>
+      <div v-if="isAuth" class="flex  items-center justify-center">
+        <UAvatar size="xs" :src="avatarUrl" />
+      </div>
+      <div v-else class="flex  items-center justify-center">
+        <UButton label="登录" variant="ghost" to="/login" />
+      </div>
+    </div>
   </nav>
 </template>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
+const isOpen = ref(false)
+
 const user = useSupabaseUser()
 const { auth } = useSupabaseClient()
 
 const redirectTo = `${useRuntimeConfig().public.baseUrl}/confirm`
 
 watchEffect(() => {
+  console.log('user', user.value)
   if (user.value)
     navigateTo('/')
+})
+onMounted(() => {
+  isOpen.value = true
 })
 </script>
 
 <template>
+  <!-- <div>
+    <UModal v-model="isOpen">
+      <div class="p-4">
+        <Placeholder class="h-48">
+          <div>
+            该页面需要登录才能访问
+          </div>
+        </Placeholder>
+      </div>
+    </UModal>
+  </div> -->
   <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <h2 class="my-6 text-center text-3xl font-extrabold u-text-white">
       Sign in to your account
